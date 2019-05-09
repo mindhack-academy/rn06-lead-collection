9fecc5ff883771b5e37f900d3f02f45103d60f2a
Rares Nicoara <raresn92@gmail.com>
2019-05-09 21:59:34 +0300

    Added LoginFormVC Added FormVC Open FormVC from LoginVC

---
+++ LeadCollection/FormVC.swift
@@ -0,0 +1,14 @@
+//
+//  FormVC.swift
+//  LeadCollection
+//
+//  Created by MindHack on 09/05/2019.
+//  Copyright © 2019 MindHack. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FormVC: UIViewController {
+    
+}
---
+++ LeadCollection/LoginVC.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  LeadCollection
+//
+//  Created by MindHack on 09/05/2019.
+//  Copyright © 2019 MindHack. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    
+    @IBOutlet weak var EmailTextField: UITextField!
+    @IBOutlet weak var PasswordTextField: UITextField!
+    
+    @IBOutlet weak var ErrorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        ErrorLabel.isHidden = true
+        ErrorLabel.text = "Email sau parola invalide"
+        
+    }
+
+    @IBAction func DoLogin(_ sender: Any) {
+        
+        
+//        print(EmailTextField.text, PasswordTextField.text)
+        
+    
+        
+        
+        
+        if EmailTextField.text == "Email@me.com",PasswordTextField.text == "pass"{
+        // autentificare validata
+            ErrorLabel.isHidden = true
+
+            if let formVC = self.storyboard?.instantiateViewController(withIdentifier: "FormVC") {
+                self.present(formVC, animated: true, completion: nil)
+
+                
+            }
+            
+            
+        } else {
+            // autentificare invalida
+            
+            ErrorLabel.isHidden = false
+            
+            
+            }
+        
+        
+    }
+    
+
+}
+
